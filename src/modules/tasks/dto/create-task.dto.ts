e22a2467c85b23029cf13f3668225f998d38cf55
Gilberto Correa <jifaco89@gmail.com>
2026-02-26 16:16:19 -0600

    Corrección de estructura de objetos

---
+++ src/modules/tasks/dto/create-task.dto.ts
@@ -0,0 +1,23 @@
+import { IsBoolean, IsInt, IsNotEmpty, IsNumber, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(100)
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(250)
+  description: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  priority: boolean;
+
+  @IsInt()
+  @IsNumber()
+  user_id: number;
+}
